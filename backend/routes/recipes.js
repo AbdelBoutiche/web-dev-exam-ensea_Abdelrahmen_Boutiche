256c06d76c55a253f2d6a5853beb60af67d67ba7
AbdelBoutiche <125290396+AbdelBoutiche@users.noreply.github.com>
2026-01-15 09:43:45 +0100

    Update recipes.js

--- backend/routes/recipes.js
+++ backend/routes/recipes.js
@@ -1,8 +1,13 @@
 import { Router } from "express"
-import { getRecipes } from "../controllers/recipesController.js"
+import { createRecipe, deleteRecipe, getRecipeById, getRecipes, searchRecipes, updateRecipe } from "../controllers/recipesController.js"
 
 const router = Router()
 
 router.get("/", getRecipes)
+router.get("/:id", getRecipeById)
+router.post("/",createRecipe)
+router.put("/:id",updateRecipe)
+router.delete("/:id",deleteRecipe)
+router.get("/:name",searchRecipes)
 
 export default router
